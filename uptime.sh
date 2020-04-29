f8c4fe68191c192da1f7159eb313183cef509fe9
Angelo Geulin <angelogeulin123@gmail.com>
2020-04-29 21:19:33 +0800

    uptime.sh: Simplify trimming and output

--- uptime.sh
+++ uptime.sh
@@ -2,7 +2,7 @@
 
 while true; do
     uptime_out="$(uptime -p)"
-    uptime_out="${uptime_out//up }"
-    echo "U   $uptime_out  "
+    uptime_out="${uptime_out/up }"
+    echo "U$uptime_out"
     sleep 60
 done
